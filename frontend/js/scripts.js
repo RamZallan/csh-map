f39a7622aeef1f5d212360f875e9ef7288fb7a7d
Ram Zallan <ramzallan@gmail.com>
2016-09-21 17:21:59 -0400

    Fixed several syntax errors.

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,68 +1,16 @@
 /* regexBldg gets building name from ID
            Ex:  nrh-3-3071  => nrh
                 fish-3-3050 => fish
-        */
-var regexBldg = /([(a-zA-Z)])\w+/g;
-/* regexNum gets room # from ID
-   Ex:  nrh-3-3071  => 3071
-        fish-3-3050 => 3050
+   regexNum gets room # from ID
+           Ex:  nrh-3-3071  => 3071
+           fish-3-3050 => 3050
 */
-var regexNum = /([^(a-zA-Z)-])\w+/g;
+var regexBldg = /([(a-zA-Z)])\w+/g,
+    regexNum = /([^(a-zA-Z)\-])\w+/g;
 
 // Used for convenience, but not neccesary
-var $modalTitle = $('#map-modal .modal-title');
-var $modalBody = $("#map-modal .modal-body");
-
-function nrhOrFish(id) {
-    /*
-    Uses regex, checks if id starts with nrh or fish, or something else
-    Example IDs:  nrh-3-3071  => isNrh
-                  fish-3-3050 => isFish
-                  sol-3-3011  => Neither
-    */
-    var bldg = id.match(regexBldg).toString();
-    var num = id.match(regexNum).toString();
-    if (bldg == "nrh") {
-        $modalTitle.css('textTransform', 'uppercase');
-        setupModal(bldg, num, outputResidents(num));
-    } else if (bldg == "fish") {
-        $modalTitle.css('textTransform', 'capitalize');
-        setupModal(bldg, num, outputResidents('F%20' + num));
-    } else {
-        console.log("ERROR: Room of id" + id + " is neither in NRH nor Fish");
-    }
-    
-}
-
-$('.room').click(function () {
-    /*
-    Uses jQuery click function,
-    gets ID of clicked element
-    and calls nrhOrFish w/ ID.
-    */
-    nrhOrFish(this.id);
-});
-
-
-
-
-function getResidents(room, callback) {
-    /*
-    Takes in room (NRH 3071 => "3071", Fish 3050 => "F%203050"),
-    makes API call, and uses callback when called to get data.
-    */
-    url = "https://map.csh.rit.edu/api/get/" + room;
-    var request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    request.onreadystatechange = function () {
-        // Uncomment line below when debugging, to check that API calls are working.
-        // console.log("GET State: " + request.readyState + " - Status: " + request.status);
-        if (request.readyState == 4 && request.status == 200) {
-            callback(request);
-        }
-    }
-    request.send(null);
-}
+var $modalTitle = $('#map-modal .modal-title'),
+    $modalBody = $("#map-modal .modal-body");
 
 function setupModal(bldg, num, residents) {
     /*
@@ -71,6 +19,24 @@ function setupModal(bldg, num, residents) {
     */
     $modalTitle.text(bldg + " " + num);
     $modalBody.text(residents);
+}
+
+function getResidents(room, callback) {
+    /*
+    Takes in room (NRH 3071 => "3071", Fish 3050 => "F%203050"),
+    makes API call, and uses callback when called to get data.
+    */
+    var url = "https://map.csh.rit.edu/api/get/" + room,
+        request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.onreadystatechange = function () {
+        // Uncomment line below when debugging, to check that API calls are working.
+        // console.log("GET State: " + request.readyState + " - Status: " + request.status);
+        if (request.readyState === 4 && request.status === 200) {
+            callback(request);
+        }
+    };
+    request.send(null);
 }
 
 function outputResidents(roomNum) {
@@ -85,11 +51,40 @@ function outputResidents(roomNum) {
         if (parsed_data[1]) {
             return (parsed_data[0] + '\n' + parsed_data[1]);
         } else if (parsed_data[0]) {
-            alert(parsed_data[0])
             return parsed_data[0];
-            
+
         } else {
             return 'No residents.';
         }
     });
 }
+
+function nrhOrFish(id) {
+    /*
+    Uses regex, checks if id starts with nrh or fish, or something else
+    Example IDs:  nrh-3-3071  => isNrh
+                  fish-3-3050 => isFish
+                  sol-3-3011  => Neither
+    */
+    var bldg = id.match(regexBldg).toString(),
+        num = id.match(regexNum).toString();
+    if (bldg === "nrh") {
+        $modalTitle.css('textTransform', 'uppercase');
+        setupModal(bldg, num, outputResidents(num));
+    } else if (bldg === "fish") {
+        $modalTitle.css('textTransform', 'capitalize');
+        setupModal(bldg, num, outputResidents('F%20' + num));
+    } else {
+        console.log("ERROR: Room of id" + id + " is neither in NRH nor Fish");
+    }
+
+}
+
+$('.room').click(function () {
+    /*
+    Uses jQuery click function,
+    gets ID of clicked element
+    and calls nrhOrFish w/ ID.
+    */
+    nrhOrFish(this.id);
+});
